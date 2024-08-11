3e4925ae8f5cb06444ea2b62157e180dfe53b582
User <al_rasim98@mail.ru>
2024-08-11 20:25:03 +0500

    Создал Spring Discovery Service

---
+++ src/main/java/org/example/marketplace_microservices/MarketplaceMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package org.example.marketplace_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceMicroservicesApplication.class, args);
+    }
+
+}
