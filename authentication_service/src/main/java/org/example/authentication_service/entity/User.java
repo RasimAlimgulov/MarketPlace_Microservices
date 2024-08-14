99fde1c3919253805dfaf2c94edd0c354ad33c62
User <al_rasim98@mail.ru>
2024-08-14 12:57:46 +0500

    всё работает, восстановил сервис authentication, надо добавить метод для добавления user-ов для тестов.

--- authentication_service/src/main/java/org/example/authentication_service/entity/User.java
+++ authentication_service/src/main/java/org/example/authentication_service/entity/User.java
@@ -1,11 +1,8 @@
 package org.example.authentication_service.entity;
-
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.example.authentication_service.Role;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
